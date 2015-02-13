c282fdfb00765224b56605e4f194d53920e00719
orilefk <olefkovitz@mac-mbp2013l.local>
2015-02-13 15:02:19 -0500

    added line breaks

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 var i = 1;
 while (i < 101) {
 	if (i % 3 == 0 && i % 5 == 0) {
-		document.write("FizzBuzz, ");
+		document.write("FizzBuzz <br>");
 	}
 	else if (i % 3 == 0) {
-		document.write("Fizz, ");
+		document.write("Fizz <br>");
 	}
 	else if (i % 5 == 0) {
-	   	document.write("Buzz, ");
+	   	document.write("Buzz <br>");
 	}
 	else {
-		document.write(i + ", ");
+		document.write(i + "<br>");
 	}
 	i++;
 }
