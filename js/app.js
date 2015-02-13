9a28307594267987919818bdc566f8c66171fa4e
orilefk <olefkovitz@mac-mbp2013l.local>
2015-02-13 14:50:31 -0500

    added commas

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 var i = 1;
 while (i < 101) {
 	if (i % 3 == 0 && i % 5 == 0) {
-		document.write("FizzBuzz");
+		document.write("FizzBuzz, ");
 	}
 	else if (i % 3 == 0) {
-		document.write("Fizz");
+		document.write("Fizz, ");
 	}
 	else if (i % 5 == 0) {
-	   	document.write("Buzz");
+	   	document.write("Buzz, ");
 	}
 	else {
-		document.write(i);
+		document.write(i + ", ");
 	}
 	i++;
 }
